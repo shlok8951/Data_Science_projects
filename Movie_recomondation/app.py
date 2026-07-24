3bf5e9826ab75252cbc8f7073578104ac37e2906
shlok8951 <shlokj771@gmail.com>
2026-07-24 10:55:30 +0530

    Update app.py

--- Movie_recomondation/app.py
+++ Movie_recomondation/app.py
@@ -115,7 +115,7 @@ def poster_grid(cards, cols=6, key_prefix="grid"):
 
             with colset[c]:
                 if poster:
-                    st.image(poster, use_container_width=True)
+                    st.image(poster, width=True)
                 else:
                     st.write("🖼️ No poster")
 
